9d8ad7d31cc34a7a481a2ede1c6e56556254fa09
David Kisluk <david.kisluk@gmail.com>
2017-05-10 07:02:43 +0000

    Add render function which mapsgamestate data to LEDs

---
+++ server/views/breadView.js
@@ -0,0 +1,38 @@
+const gpio = require('rpi-gpio');
+const config = require('./config.js');
+const { Cell } = require('../../shared/enums.js');
+
+const on = pin => gpio.write(pin, true);
+const off = pin => gpio.write(pin, false);
+const setup = () => {
+  config.forEach((cell) => {
+    gpio.setup(cell.red, gpio.DIR_OUT);
+    gpio.setup(cell.white, gpio.DIR_OUT);
+  });
+};
+
+setup();
+
+const render = (gameState) => {
+  gameState.forEach((row, y) => {
+    row.forEach((color, x) => {
+      const index = (y * 3) + x;
+
+      switch (color) {
+        case Cell.RED:
+          off(config[index].white);
+          on(config[index].red);
+          break;
+        case Cell.WHITE:
+          off(config[index].red);
+          on(config[index].white);
+          break;
+        default:
+          off(config[index].red);
+          off(config[index].white);
+      }
+    });
+  });
+};
+
+module.exports = render;
---
+++ server/views/config.js
@@ -0,0 +1,38 @@
+module.exports = Object.freeze([
+  {
+    red: 8,
+    white: 10,
+  },
+  {
+    red: 12,
+    white: 16,
+  },
+  {
+    red: 18,
+    white: 22,
+  },
+  {
+    red: 32,
+    white: 36,
+  },
+  {
+    red: 38,
+    white: 40,
+  },
+  {
+    red: 7,
+    white: 11,
+  },
+  {
+    red: 13,
+    white: 15,
+  },
+  {
+    red: 29,
+    white: 31,
+  },
+  {
+    red: 33,
+    white: 35,
+  },
+]);
