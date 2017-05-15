aef0135100a3920933c314d99f6e80c97b0ea732
David Kisluk <david.kisluk@gmail.com>
2017-05-14 20:47:46 -0400

    Add comments to client/app.js

--- client/app.js
+++ client/app.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || process.env.NODE_PORT || 1337;
 
 const SERVER_URL = 'http://localhost:3000';
 
+/**
+ * Helper function that wraps `request`. Provides full URL for a given route.
+ * Also format as the `gameState` as a string if provided so that it can be
+ * sent in the body of the HTTP request.
+ *
+ * @param {string} route - The name of the route exposed by the server.
+ * @param {number[][]} gameState - The state of the Tic-Tac-Toe game.
+ * @param {function} callback - Function to call once the request finishes.
+ */
 const postToServer = (route, gameState, callback) => {
   const requestOptions = {
     method: 'POST',
@@ -19,13 +28,23 @@ const postToServer = (route, gameState, callback) => {
     requestOptions.form = { gameState: JSON.stringify(gameState) };
   }
 
-  // console.log(requestOptions);
-
   request(requestOptions, callback);
 };
 
+/**
+ * Converts string to gameState object.
+ *
+ * @param {string} body - The body of an HTTP response.
+ * @returns {number[][]} - The state of the Tic-Tac-Toe game.
+ */
 const parseGameState = body => JSON.parse(JSON.parse(body).gameState);
 
+/**
+ * Chooses best move given current state of the game, then sends that move
+ * to the sever.
+ *
+ * @param {number[][]} gameState - The state of the Tic-Tac-Toe game.
+ */
 const makeMove = (gameState) => {
   const nextMove = TicTacToe.findBestMove(gameState, Cell.WHITE);
   console.log(nextMove);
@@ -36,6 +55,7 @@ const makeMove = (gameState) => {
   }
 
   postToServer('/makeMove', nextMove, (error, response, body) => {
+    // Continue making moves until the server responds that game is over.
     if (!JSON.parse(body).gameOver) {
       const newGameState = parseGameState(body);
       makeMove(newGameState);
@@ -43,6 +63,10 @@ const makeMove = (gameState) => {
   });
 };
 
+/**
+ * Entry point to the client application. Sends request to the server to
+ * start a new game of Tic-Tac-Toe.
+ */
 app.listen(port, () => {
   console.log(`Client PI listening on port ${port}!`);
 
